246fc595256abf60cc578a0d6ce2fc2f3491966e
cttedwards <charles.edwards@niwa.co.nz>
2014-06-04 17:47:24 +1200

    MOD: modified histplot

--- R/histplot.R
+++ R/histplot.R
@@ -1,52 +1,98 @@
 
 #{{{ histogram diagnostic plot
-setGeneric("histplot", function(.Object,pars, ...) standardGeneric("histplot"))
-setMethod("histplot",signature=c("bdm"), function(.Object,pars) {
+setGeneric("histplot", function(.Object,pars,inc_warmup=FALSE, ...) standardGeneric("histplot"))
+setMethod("histplot",signature=c("bdm"), function(.Object,pars,inc_warmup=FALSE) {
   
   require(ggplot2)
   
-  if(missing(pars) & .Object@default_model) 
-    pars <- c('r','logK','lp__')
+  if(missing(pars)) {
+    if(.Object@default_model) 
+      pars <- c('r','logK','lp__')
+    else stop('must define pars for non-default model')
+  }
   
-  loc <- array(dim=c(length(pars),2),dimnames=list(pars,c('i','j')))
+  #############################################################
+  # code for extraction of iterations from object@trace_array #
+  #############################################################
+  dfr <- data.frame(variable=NULL,chain=NULL,value=NULL)
+  
   for(par in pars) {
     m <- regexpr('\\[.+\\]',par)
     if(m>0) {
-      loc[par,'i'] <- match(substr(par,1,m-1),names(.Object@trace))
-      m <- m + 1
-      attributes(m)$match.length <- attributes(m)$match.length - 2
-      loc[par,'j'] <- as.numeric(regmatches(par,m))
-    } else {
-      loc[par,'i'] <- match(par,names(.Object@trace))
-    }
-  }
-  
-  dfr <- data.frame(variable=NULL,value=NULL)
-  for(par in pars) {
-    
-    i <- loc[par,'i']
-    j <- loc[par,'j']
-    
-    if(length(dim(.Object@trace[[i]]))>1) {
-      list.tmp <- lapply(apply(.Object@trace[[i]],2,function(x) list(x)),unlist)
-      if(is.na(j)) {
-        for(j in 1:length(list.tmp)) {
-          dfr <- rbind(dfr,data.frame(variable=paste(names(.Object@trace)[i],'[',j,']',sep=''),value=list.tmp[[j]]))
-        }
-      } else {
-        dfr <- rbind(dfr,data.frame(variable=paste(names(.Object@trace)[i],'[',j,']',sep=''),value=list.tmp[[j]]))
+      i <- match(par,dimnames(.Object@trace_array)$parameters)
+      if(!is.na(i)) {
+        dfr.tmp <- melt(.Object@trace_array[,,i])
+        dfr <- rbind(dfr,data.frame(variable=dimnames(.Object@trace_array)$parameters[i],iteration=dfr.tmp$iterations,chain=dfr.tmp$chains,value=dfr.tmp$value))
       }
     } else {
-      dfr <- rbind(dfr,data.frame(variable=names(.Object@trace)[i],value=.Object@trace[[i]]))
+      mm <- 0
+      for(parname in dimnames(.Object@trace_array)$parameters) {
+        m  <- regexpr(par,parname)
+        if(m>0) { 
+          i <- match(parname,dimnames(.Object@trace_array)$parameters)
+          dfr.tmp <- melt(.Object@trace_array[,,i])
+          dfr <- rbind(dfr,data.frame(variable=parname,iteration=dfr.tmp$iterations,chain=dfr.tmp$chains,value=dfr.tmp$value)) 
+          mm <- mm + 1
+        } else {
+          if(mm>0) break
+        }
+      }
     }
   }
+  if(!nrow(dfr)>0) stop('parameter not found\n')
+  
+  dfr$chain <- unlist(lapply(strsplit(as.character(dfr$chain),split=':'),function(x) x[2]))
+  
+  if(!inc_warmup) dfr <- subset(dfr,iteration>.Object@warmup)
   
   gg <- ggplot(dfr) + 
-          geom_histogram(aes(x=value),position='identity') + 
+          geom_histogram(aes(x=value,fill=chain),position='stack') + 
           facet_wrap(~variable,scales='free_x') +
           xlab('Parameter value') +
           ylab('Sample counts') + theme_bw()
   
-  suppressMessages(print(gg))
+  ########################################################
+  # code for extraction of iteractions from object@trace #
+  ########################################################
+  #loc <- array(dim=c(length(pars),2),dimnames=list(pars,c('i','j')))
+  #for(par in pars) {
+  #  m <- regexpr('\\[.+\\]',par)
+  #  if(m>0) {
+  #    loc[par,'i'] <- match(substr(par,1,m-1),names(.Object@trace))
+  #    m <- m + 1
+  #    attributes(m)$match.length <- attributes(m)$match.length - 2
+  #    loc[par,'j'] <- as.numeric(regmatches(par,m))
+  #  } else {
+  #    loc[par,'i'] <- match(par,names(.Object@trace))
+  #  }
+  #}
+  #
+  #dfr <- data.frame(variable=NULL,value=NULL)
+  #for(par in pars) {
+  #  
+  #  i <- loc[par,'i']
+  #  j <- loc[par,'j']
+  #  
+  #  if(length(dim(.Object@trace[[i]]))>1) {
+  #    list.tmp <- lapply(apply(.Object@trace[[i]],2,function(x) list(x)),unlist)
+  #    if(is.na(j)) {
+  #      for(j in 1:length(list.tmp)) {
+  #        dfr <- rbind(dfr,data.frame(variable=paste(names(.Object@trace)[i],'[',j,']',sep=''),value=list.tmp[[j]]))
+  #      }
+  #    } else {
+  #      dfr <- rbind(dfr,data.frame(variable=paste(names(.Object@trace)[i],'[',j,']',sep=''),value=list.tmp[[j]]))
+  #    }
+  #  } else {
+  #    dfr <- rbind(dfr,data.frame(variable=names(.Object@trace)[i],value=.Object@trace[[i]]))
+  #  }
+  #}
+  #
+  #gg <- ggplot(dfr) + 
+  #        geom_histogram(aes(x=value),position='identity') + 
+  #        facet_wrap(~variable,scales='free_x') +
+  #        xlab('Parameter value') +
+  #        ylab('Sample counts') + theme_bw()
+  
+  suppressMessages(suppressWarnings(print(gg)))
 })
 #}}}
