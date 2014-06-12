07b83fa208179e88f1d0e3400c0f938a8aaa50cb
cttedwards <charles.edwards@niwa.co.nz>
2014-06-12 14:17:48 +1200

    DEL: manually resolved conflict
    Should be ok now to start afresh with kobe example

--- tests/albacore.r
+++ tests/albacore.r
@@ -10,10 +10,8 @@ rm(list=ls())
 # install and load bdm package
 #install.packages("bdm",contriburl="file://niwa.local/Groups/wellington/niwafisheries/R/",type="win.binary",repos=NULL)
 install.packages("C:/PROJECTS/SOFTWARE/OpenSource/bdm_1.0.zip", repos = NULL)
-install.packages("C:/PROJECTS/SOFTWARE/OpenSource/kobe_1.3.2.zip", repos = NULL)
 
 library(bdm)
-help(package='bdm')
 
 # load data
 data(albacore)
@@ -36,19 +34,12 @@ mdl
 mdl <- compile_bdm(mdl)
 
 # mcmc fit
-<<<<<<< HEAD
-<<<<<<< HEAD
-mdl <- fit(mdl,dat,iter=2000,thin=10,run='run_1')
-=======
-mdl <- fit(mdl,dat,iter=20000,thin=10)
->>>>>>> parent of 6a78128... MOD: add kobe plots to IO albacore example
-=======
-mdl <- fit(mdl,dat,iter=20000,thin=10)
->>>>>>> parent of 6a78128... MOD: add kobe plots to IO albacore example
+mdl <- fit(mdl,dat,iter=20000,thin=100)
 traceplot(mdl,pars=c('r','logK'))
 histplot(mdl,pars=c('r','logK'))
 
 #######
 # END #
 #######
+
 
