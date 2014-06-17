65191ec1787dcf46b228689a84263ad78d7d9461
cttedwards <charles.edwards@niwa.co.nz>
2014-06-18 09:05:47 +1200

    MOD: tests for derivation of MPD q

--- tests/test_lognormal_errors.r
+++ tests/test_lognormal_errors.r
@@ -1,28 +1,49 @@
 
 # test log-normal error probability assumptions
 
-# assume both process and observation error
-# share the same (large) sigma
-sigma <- 0.4
-sigma2 <- sigma^2
+rm(list=ls())
+
+# process and observation error
+sigmap  <- 0.2
+sigmap2 <- sigmap^2
+
+sigmao  <- 0.4
+sigmao2 <- sigmao^2
 
 # large number of years (iterations)
 nyr <- 1e4
 
 # dynamics
-xdevs <- rlnorm(nyr,log(1)-sigma2/2,sigma)
+xdevs <- rlnorm(nyr,log(1)-sigmap2/2,sigmap)
 x <- (1-sin(seq(0,pi/3,length=nyr))) * xdevs
-mean(xdevs) # == 1
+mean(xdevs) # E[xdevs] == 1;
 min(x) # > 0
 
 # catchability
-q <- 0.9
+q <- 1.0
 
 # simulate observations
-I <- rlnorm(length(x),log(q*x)-sigma2/2,sigma)
-mean(I - q*x) # == 0
+I <- rlnorm(length(x),log(q*x)-sigmao2/2,sigmao)
+mean(I - q*x) # E[I] = E[q.B] => E[I] - [q.B] == 0
 min(I) # > 0
-qqnorm(log(I/(q*x))) # normal
+qqnorm(log(I/(q*x))) # I/(q.B) is log-normal, therefore log(I/(q.B)) is normal
+
+# check analytical derivation of q
+# mle
+loglx <- expression(-c-((log(x) - log(q * B) + sigma^2/2)^2)/(2*sigma^2))
+D(loglx,'q')
+
+(qhat <- exp(mean(log(I/x)) + sigmao2/2))
+
+mean(I - qhat*x) # == 0
+
+# mpd with uniform prior on log(q)
+loglx <- expression(-c-((log(x) - log(q * B) + sigma^2/2)^2)/(2*sigma^2) + log(1/q))
+D(loglx,'q')
+
+(qhat <- exp(mean(log(I/x)) - sigmao2/2))
+
+mean(I - qhat*x) # != 0
 
 # plot subset
 loc <- sort(sample(nyr,200))
