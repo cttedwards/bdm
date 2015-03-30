4448e9d2989516d304350634acd8934dacb5e3d0
cttedwards <charles.edwards@niwa.co.nz>
2015-03-31 11:58:11 +1300

    ADD: added dynplot(), status() and theme_bdm()

---
+++ R/status.R
@@ -0,0 +1,13 @@
+
+#{{{ refrence point accessor function
+setGeneric("status", function(object, ...)
+  standardGeneric("status"))
+setMethod("status",signature("bdm"),
+          function(object) {
+            return(list(current_biomass=object@trace$current_biomass,
+                        current_depletion=object@trace$current_depletion,
+                        current_harvest_rate=object@trace$current_harvest_rate)
+                   )
+          }
+)
+#}}}
