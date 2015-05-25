624c258e33528db6eb432d0d42b0278da13edb6c
Charles <Edwards>
2015-05-26 09:51:48 +1200

    ADD: index fit diagnostic function

--- R/idxplot.R
+++ R/idxplot.R
@@ -1,15 +1,15 @@
 
 #{{{ diagnostic plot
-setGeneric("idxplot", function(.Object, ...) standardGeneric("idxplot"))
-setMethod("idxplot",signature=c("bdm"), function(.Object, ...) {
+setGeneric("idxplot", function(.Object, labels, ...) standardGeneric("idxplot"))
+setMethod("idxplot",signature=c("bdm"), function(.Object, labels, ...) {
   
   nidx <- .Object@data[['I']]
   time <- .Object@data[['time']]
   nsamples <- .Object@nsamples
   
-  # color blind palette
-  plt <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-  
+  if(missing(labels))
+      labels <- paste('index:',1:nidx,sep='')
+      
   #######################################################
   # code for extraction of iterations from object@trace #
   # (list of parameter arrays with all chains combined) #
@@ -20,28 +20,26 @@ setMethod("idxplot",signature=c("bdm"), function(.Object, ...) {
   for(i in 1:nidx) {
       
       idx.dfr <- melt(idx.arr[,,i])
-      idx.dfr <- data.frame(idx.dfr,label=paste('index:',i,sep=''))
+      idx.dfr <- data.frame(idx.dfr,label=labels[i])
       
       dfr <- rbind(dfr,idx.dfr)
   }
   
-  gg <- ggplot(dfr,aes(time,value)) + 
-      #stat_summary(fun.ymin=function(x) quantile(x,0.025),fun.ymax=function(x) quantile(x,0.975),geom='ribbon',alpha=0.3) +
-      #stat_summary(fun.y=function(x) mean(x),geom='line',lwd=1.5) +
-      labs(x='Time',y='Predicted Value') +
-      .theme_bdm()
+  gg <- ggplot() + 
+      stat_summary(data=dfr,aes(time,value),fun.ymin=function(x) quantile(x,0.025),fun.ymax=function(x) quantile(x,0.975),geom='ribbon',alpha=0.3) +
+      stat_summary(data=dfr,aes(time,value),fun.y=function(x) mean(x),geom='line',lwd=1.5) +
+      labs(x='Time',y='Predicted Index') +
+      .theme_bdm() %+replace% theme(legend.position="none")
   
   dfr.empirical <- .Object@data$index
-  dimnames(dfr.empirical) <- list(time = .Object@data$time, label = paste('index:',1:nidx,sep=''))
+  dimnames(dfr.empirical) <- list(time = .Object@data$time, label = labels)
   dfr.empirical[dfr.empirical < 0] <- NA
   dfr.empirical <- melt(dfr.empirical)
   
-  for(i in 1:nidx) {
-    gg <- geom_point(data=dfr.empirical,aes(time,value)) + 
-          geom_line(data=dfr.empirical,aes(time,value))
-  }
-  
-  if(length(nidx)>1)
+  gg <- gg + geom_point(data=dfr.empirical,aes(time,value,col=label), size=4) + 
+      geom_line(data=dfr.empirical,aes(time,value,col=label), size=1.5)
+ 
+  if(nidx>1)
       gg <- gg + facet_grid(label~.)
   
   return(gg)
