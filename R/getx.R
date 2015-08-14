08fabf4226893356d1ffa6cf5ee5cbed1103bb37
Charles Edwards <charles.edwards@niwa.co.nz>
2015-08-14 15:40:01 +1200

    created getr(), getlogK() and getx()

---
+++ R/getx.R
@@ -0,0 +1,17 @@
+getx <- function(data, r, logK) {
+    
+    cc <- data$harvest
+    tt <- length(cc)
+    bm <- numeric(tt)
+    
+    ll <- 1e-4
+    
+    bm[1] <- 1
+    for(t in 1:tt) 
+        bm[t+1] <- max(bm[t] + r*bm[t]*(1 - bm[t]) - cc[t]/exp(logK),ll)
+    bm <- bm[-length(bm)]
+    
+    init.x <- bm
+    
+    init.x
+}
