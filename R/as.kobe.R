3fccd9dafe6be06286767654220828f1d70ea403
Charles Edwards <charles.edwards@niwa.co.nz>
2015-08-14 12:38:54 +1200

    fixed some deficiences due to non-export of functions from kobe package

--- R/as.kobe.R
+++ R/as.kobe.R
@@ -10,7 +10,7 @@ setMethod("as.kobe",signature=c("bdm"),function(.Object,projection,
     if(missing(projection)) {
       
       res <- .read_bdm(.Object)
-		  res <- .io_bdm(res,what=what,prob=prob,nwrms=nwrms,year=year)
+      res <- .io_bdm(res,what=what,prob=prob,nwrms=nwrms,year=year)
       
     } else {
 
@@ -39,24 +39,24 @@ setMethod("as.kobe",signature=c("bdm"),function(.Object,projection,
   iters <- 1:niter
   
   res <- .Object@trace$biomass
-  dimnames(res) <- list(iter=iters,year=years)
+  dimnames(res) <- list(iter = iters, year = years)
   
-  res <- melt(res,value.name='stock')
-  res <- data.frame(res,harvest=melt(.Object@trace$harvest_rate)$value)
+  res <- melt(res,value.name = 'stock')
+  res <- data.frame(res,harvest = melt(.Object@trace$harvest_rate)$value)
   
   bmsy <- .Object@trace$biomass_at_msy
   fmsy <- .Object@trace$harvest_rate_at_msy
   
-  ref <- data.frame(iter=iters,bmsy=bmsy,fmsy=fmsy)
+  ref <- data.frame(iter = iters, bmsy = bmsy, fmsy = fmsy)
   
-  res <- merge(res,ref,by="iter")
+  res <- merge(res,ref,by = "iter")
   
   res$stock   <- res$stock/res$bmsy
   res$harvest <- res$harvest/res$fmsy
   
-  if(length(.Object@run)>0) {
+  if(length(.Object@run) > 0) {
     run <- .Object@run
-    res <- data.frame(run=run,res)
+    res <- data.frame(run = run,res)
   }
 
   return(res)
@@ -108,38 +108,57 @@ setMethod("as.kobe",signature=c("bdm"),function(.Object,projection,
     
     if (is.null(year)) pts <- max(res$year)
       
-    trks. =NULL
-    pts.  =NULL
-    smry. =NULL
-    wrms. =NULL
-    sims. =NULL
+    trks. = NULL
+    pts.  = NULL
+    smry. = NULL
+    wrms. = NULL
+    sims. = NULL
         
     if ("trks" %in% what){ 
-      stock  =ddply(res,.(year),function(x) quantile(x$stock,    prob, na.rm=TRUE))
-      harvest=ddply(res,.(year),function(x) quantile(x$harvest,  prob, na.rm=TRUE))
-      trks.=data.frame(melt(stock,id.vars="year"),"harvest"=melt(harvest,id.vars="year")[,3])
+      stock   = ddply(res,.(year),function(x) quantile(x$stock,    prob, na.rm=TRUE))
+      harvest = ddply(res,.(year),function(x) quantile(x$harvest,  prob, na.rm=TRUE))
+      trks. = data.frame(melt(stock,id.vars="year"),"harvest"=melt(harvest,id.vars="year")[,3])
       names(trks.)[c(2,3)]=c("Percentile","stock")}
     
     if ("pts" %in% what)
-      pts.=res[res$year %in% pts,]
+      pts. = res[res$year %in% pts,]
     
     if ("sims" %in% what)
-      sims.=res
+      sims. = res
     
-    if ("smry" %in% what)
-       smry. =ddply(data.frame(res,kobeP(as.numeric(res$stock),as.numeric(res$harvest))),
-                           .(year), function(x) data.frame(stock      =median(x$stock,       na.rm=TRUE),
-                                                           harvest    =median(x$harvest,     na.rm=TRUE),
-                                                           red        =mean(  x$red,         na.rm=TRUE),
-                                                           yellow     =mean(  x$yellow,      na.rm=TRUE),
-                                                           green      =mean(  x$green,       na.rm=TRUE),
-                                                           overFished =mean(  x$overFished,  na.rm=TRUE),
-                                                           overFishing=mean(  x$overFishing, na.rm=TRUE)))
+    if ("smry" %in% what) {
+        
+        kobe.quadrant <- function(stock, harvest) {
+            
+            b =   pmax(pmin(as.integer(stock),  1),0)
+            f = 1-pmax(pmin(as.integer(harvest),1),0)
+            p = f*b
+            collapsed = (1-b)*(1-f)
+            
+            red    = collapsed
+            green  = p
+            yellow = 1-p-collapsed
+            
+            overFished  = 1-b
+            overFishing = 1-f  
+            
+            data.frame(red=red,green=green,yellow=yellow,overFished=overFished,overFishing=overFishing)
+        }
+    
+       smry. = ddply(data.frame(res,kobe.quadrant(as.numeric(res$stock),as.numeric(res$harvest))),
+                           .(year), function(x) data.frame(stock      = median(x$stock,       na.rm=TRUE),
+                                                           harvest    = median(x$harvest,     na.rm=TRUE),
+                                                           red        = mean(  x$red,         na.rm=TRUE),
+                                                           yellow     = mean(  x$yellow,      na.rm=TRUE),
+                                                           green      = mean(  x$green,       na.rm=TRUE),
+                                                           overFished = mean(  x$overFished,  na.rm=TRUE),
+                                                           overFishing= mean(  x$overFishing, na.rm=TRUE)))
+    }
     
     if ("wrms" %in% what)
-      wrms.=res[res$iter %in% sample(unique(res$iter),nwrms),c("iter","year","stock","harvest")]
+      wrms. = res[res$iter %in% sample(unique(res$iter),nwrms),c("iter","year","stock","harvest")]
     
-    return(list(trks=trks.,pts=pts.,smry=smry.,wrms=wrms.,sims=sims.))
+    return(list(trks = trks., pts = pts., smry = smry., wrms = wrms., sims = sims.))
 
 }
 #}
