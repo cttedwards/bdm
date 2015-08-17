2582027bd503a811abf173a5f55ced0cf1ee9176
Charles Edwards <charles.edwards@niwa.co.nz>
2015-08-18 10:53:05 +1200

    created getr S3 generic

---
+++ R/getr-generic.R
@@ -0,0 +1,8 @@
+#'
+#' @title S3 generic function to extract log-normal distribution parameters for the prior on intrinsic growth
+#' 
+#' @export
+#' 
+
+# S3 generic function
+getr <- function(x, ...) UseMethod("getr")
