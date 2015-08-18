6d6f17a9f9fcc08f80617302a72e9018b5113d33
Charles Edwards <charles.edwards@niwa.co.nz>
2015-08-18 12:01:55 +1200

    organised rdat code

---
+++ R/rdat-class.R
@@ -0,0 +1,5 @@
+#' @title rdat-class definition
+#' 
+#' @description rdat class definition
+#'
+setClass("rdat", representation(iter = "numeric", amax = "numeric", sr = "character", lhdat = "list"))
