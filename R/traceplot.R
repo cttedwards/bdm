020e08b5eda226c3a33b48409c2ae5d656ea84c0
Charles Edwards <charles.edwards@niwa.co.nz>
2015-08-18 16:27:42 +1200

    tidied up code

--- R/traceplot.R
+++ R/traceplot.R
@@ -1,64 +1,68 @@
-
-#{{{ plot traces
-setMethod("traceplot",signature="bdm",function(object, pars, inc_warmup = TRUE, ask = FALSE, ...) {
+#'
+#' @title Traceplot for bdm MCMC output
+#' 
+#' @export
+#' @include ggtheme.R
+#' 
+setMethod("traceplot", signature  =  "bdm",function(object, pars, inc_warmup = TRUE, ask = FALSE, ...) {
   
-  if(missing(pars)) {
-    if(object@default_model) 
+  if (missing(pars)) {
+    if (object@default_model) 
       pars <- c('r','logK','lp__')
     else stop('must define pars for non-default model')
   }
   
-  dfr <- data.frame(variable=NULL,chain=NULL,value=NULL)
+  dfr <- data.frame(variable = NULL,chain = NULL,value = NULL)
   
   # extract paramater values from object@trace_array
-  for(par in pars) {
+  for (par in pars) {
     m <- regexpr('\\[.+\\]',par)
-    if(m>0) {
+    if (m>0) {
       i <- match(par,dimnames(object@trace_array)$parameters)
-      if(!is.na(i)) {
+      if (!is.na(i)) {
         dfr.tmp <- melt(object@trace_array[,,i])
-	      if(ncol(dfr.tmp)>2) {
-			    dfr <- rbind(dfr,data.frame(variable=dimnames(object@trace_array)$parameters[i],iteration=dfr.tmp$iterations,chain=dfr.tmp$chains,value=dfr.tmp$value))
+	      if (ncol(dfr.tmp)>2) {
+			    dfr <- rbind(dfr,data.frame(variable = dimnames(object@trace_array)$parameters[i],iteration = dfr.tmp$iterations,chain = dfr.tmp$chains,value = dfr.tmp$value))
 		    } else {
-			    dfr <- rbind(dfr,data.frame(variable=dimnames(object@trace_array)$parameters[i],iteration=1:dim(dfr.tmp)[1],chain='chain:1',value=dfr.tmp$value))
+			    dfr <- rbind(dfr,data.frame(variable = dimnames(object@trace_array)$parameters[i],iteration = 1:dim(dfr.tmp)[1],chain = 'chain:1',value = dfr.tmp$value))
 		    }
 	    }
     } else {
       mm <- 0
-      for(parname in dimnames(object@trace_array)$parameters) {
+      for (parname in dimnames(object@trace_array)$parameters) {
         m  <- regexpr(par,parname)
-        if(m>0) { 
+        if (m>0) { 
           i <- match(parname,dimnames(object@trace_array)$parameters)
           dfr.tmp <- melt(object@trace_array[,,i])
-		      if(ncol(dfr.tmp)>2) {
-			      dfr <- rbind(dfr,data.frame(variable=parname,iteration=dfr.tmp$iterations,chain=dfr.tmp$chains,value=dfr.tmp$value)) 
+		      if (ncol(dfr.tmp)>2) {
+			      dfr <- rbind(dfr,data.frame(variable = parname,iteration = dfr.tmp$iterations,chain = dfr.tmp$chains,value = dfr.tmp$value)) 
           } else {
-			      dfr <- rbind(dfr,data.frame(variable=parname,iteration=1:dim(dfr.tmp)[1],chain='chain:1',value=dfr.tmp$value)) 
+			      dfr <- rbind(dfr,data.frame(variable = parname,iteration = 1:dim(dfr.tmp)[1],chain = 'chain:1',value = dfr.tmp$value)) 
 		      }
 		      mm <- mm + 1
         } else {
-          if(mm>0) break
+          if (mm>0) break
         }
       }
     }
   }
-  if(!nrow(dfr)>0) stop('parameter not found\n')
+  if (!nrow(dfr)>0) stop('parameter not found\n')
   
   # extract chain numbering
-  dfr$chain <- unlist(lapply(strsplit(as.character(dfr$chain),split=':'),function(x) x[2]))
+  dfr$chain <- unlist(lapply(strsplit(as.character(dfr$chain),split = ':'),function(x) x[2]))
   
-  if(!inc_warmup) 
+  if (!inc_warmup) 
     dfr <- subset(dfr,iteration>object@warmup)
   
   gg <- ggplot(dfr)
     
-  if(inc_warmup) 
-    gg <- gg + geom_vline(xintercept=object@warmup,linetype='longdash',col='grey50')
+  if (inc_warmup) 
+    gg <- gg + geom_vline(xintercept = object@warmup,linetype = 'longdash',col = 'grey50')
   
-  gg <- gg + geom_line(aes(x=iteration,y=value,col=chain)) + 
-    facet_wrap(~variable,scales='free_y') +
+  gg <- gg + geom_line(aes(x = iteration,y = value,col = chain)) + 
+    facet_wrap(~variable,scales = 'free_y') +
     xlab('Iteration') +
-    ylab('Parameter value') + .theme_bdm()
+    ylab('Parameter value') + ggtheme()
   
   return(gg)
   
