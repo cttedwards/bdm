cb8db62bcc8dcb692d184ab916863ac54131863a
Charles Edwards <charles.edwards@niwa.co.nz>
2015-08-18 16:12:59 +1200

    created compile.bdm S3 function
    potential conflict with compiler::compile

---
+++ R/compile.R
@@ -0,0 +1,28 @@
+#'
+#' @title Compile stan model in bdm object
+#' 
+#' @export
+compile <- function(x, ...) UseMethod("compile")
+#'
+#' @rdname compile
+#' @export
+compile.bdm <- function(.Object) {
+    
+  if (.Object@path != 'default_model' & .Object@path != 'local_declaration') {
+    tmp <- stan_model(file = .Object@path)
+    .Object@model_code <- tmp@model_code
+  } else {
+    if (length(.Object@model_code) > 0) {
+          tmp <- stan_model(model_code = .Object@model_code)
+          .Object@path  <- ifelse(.Object@default_model,'default_model','local_declaration')
+    } else {
+        stop('compilation error; check .Object@path and .Object@model_code\n')
+    }
+  }
+  
+  .Object@model_cpp  <- tmp@model_cpp
+  .Object@dso        <- tmp@dso
+  
+  .Object
+  
+}
