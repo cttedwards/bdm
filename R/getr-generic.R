0e1c077aec2b9b914cf2edb3912c298e04def868
Charles Edwards <charles.edwards@niwa.co.nz>
2015-08-31 13:15:07 +1200

    updated getr()
    combined getr() generic with getr.bdm() and getr.rprior() to simplify
    documentation

--- R/getr-generic.R
+++
@@ -1,7 +0,0 @@
-#'
-#' @title S3 generic function to extract log-normal distribution parameters for the prior on intrinsic growth
-#' 
-#' @export
-#' 
-# S3 generic function
-getr <- function(x, ...) UseMethod("getr")
