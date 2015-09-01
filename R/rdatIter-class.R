de884ded8fedaa095e1d863562a2e3daae7d1743
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-01 12:15:15 +1200

    roxygen for rdatIter class

--- R/rdatIter-class.R
+++ R/rdatIter-class.R
@@ -1,5 +1,1 @@
-#' @title rdatIter-class definition
-#' 
-#' @description rdatIter class definition
-#'
 setClass("rdatIter", representation(amax = "numeric", sr = "character", lhdat = "list"))
--- R/rdatIter.R
+++ R/rdatIter.R
@@ -1,12 +1,27 @@
-#' @title Create rdatIter object
 #' 
-#' @description Initialise rdatIter class object
-#' 
-#' @export
-#' 
-#' @include rdatIter-initialize.R
+#' @include rdatIter-class.R
 #'
 #{{{
-# constructor
 rdatIter <- function(amax, sr, ...) new("rdatIter", amax, sr, ...)
 #}}}
+setMethod("initialize", "rdatIter", function(.Object, amax, sr, ...) {
+    
+    if (!missing(amax)) {
+        
+        .Object@amax <- amax
+        .Object@lhdat[['survivorship']] <- vector('numeric',amax)
+        .Object@lhdat[['M']]            <- vector('numeric',amax)
+        .Object@lhdat[['size']]         <- vector('numeric',amax)
+        .Object@lhdat[['mass']]         <- vector('numeric',amax)
+        .Object@lhdat[['maturity']]     <- vector('numeric',amax)
+        
+    }
+    
+    if (!missing(sr)) {
+        .Object@sr           <- sr
+        .Object@lhdat[['h']] <- numeric(1)
+    }
+    
+    .Object
+    
+})
