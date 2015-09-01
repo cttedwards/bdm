9dde1302012e490d819391c3fa44b8e3c610a618
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-01 12:16:10 +1200

    roxygen for iterate()

--- R/iterate.R
+++ R/iterate.R
@@ -1,13 +1,29 @@
 #'
-#' @title Add iterations to existing rdat object
+#' Add iterations to existing \code{rdat} object
+#' 
+#' This function will add an iteration dimension to the life-history data contained in an \code{rdat} object.
+#' 
+#' @param object existing \code{rdat} object
+#' @param iter number of iterations
+#' 
+#' @return Returns an \code{rdat} object with life-history data replicated \code{iter} times
+#' 
+#' @examples
+#' # initialise data object for calculation of r
+#' dat <- rdat(amax = 30, iter = 1)
+#' 
+#' # to generate monte-carlo iterations first either 
+#' # re-initialise 'rdat' object using more iterations or 
+#' # iterate existing object:
+#' dat <- iterate(dat, iter = 200)
 #' 
 #' @export
-#' @include rdat-initialize.R
+#' @include rdat-class.R
 #' 
 #{{{ 
-setGeneric("iterate", function(x, iter, ...)
+setGeneric("iterate", function(object, iter, ...)
   standardGeneric("iterate"))
-setMethod("iterate", signature(x = "rdat",iter = "numeric"),
-          function(x, iter) new('rdat', rdat = x, iter = iter)
+setMethod("iterate", signature(object = "rdat",iter = "numeric"),
+          function(object, iter) new('rdat', rdat = object, iter = iter)
 )
 #}}}
