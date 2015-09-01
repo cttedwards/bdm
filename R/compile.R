d23cb95a06b38b7a6202f2e3a7b20484dfd82b6c
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-02 09:37:51 +1200

    added roxygen comments and reading of stan file
    should now read stan file from path argument and load into model_code
    slot even when it is not compiled

--- R/compile.R
+++ R/compile.R
@@ -1,28 +1,47 @@
 #'
-#' @title Compile stan model in bdm object
+#' Compile stan model code in \code{bdm} object
+#' 
+#' This function calls \code{rstan::stan_model} to compile the model
+#' 
+#' @param object a \code{bdm} class object that contains the model code in \code{object@@model_code}
+#' @param ... additional arguments to \code{stan_model}
+#' 
+#' @examples
+#' # initialise default model object
+#' mdl <- bdm(compile = FALSE)
+#' 
+#' # compile Stan model code
+#' mdl <- compile(mdl)
+#' 
+#' @return a \code{bdm} class object with a compiled DSO in \code{object@@dso}
+#' 
+#' @seealso See the \href{https://github.com/stan-dev/rstan/wiki/RStan-Getting-Started}{RStan Quick Start Guide} to ensure that \code{\link{rstan}} is installed correctly before compilation.
 #' 
 #' @export
-compile <- function(x, ...) UseMethod("compile")
+#' 
+compile <- function(object, ...) UseMethod("compile", ...)
 #'
 #' @rdname compile
+#' 
 #' @export
-compile.bdm <- function(.Object) {
+#' 
+compile.bdm <- function(object, ...) {
     
-  if (.Object@path != 'default_model' & .Object@path != 'local_declaration') {
-    tmp <- stan_model(file = .Object@path)
-    .Object@model_code <- tmp@model_code
+  if (object@path != 'default_model' & object@path != 'local_declaration') {
+    tmp <- stan_model(file = object@path, ...)
+    object@model_code <- tmp@model_code
   } else {
-    if (length(.Object@model_code) > 0) {
-          tmp <- stan_model(model_code = .Object@model_code)
-          .Object@path  <- ifelse(.Object@default_model,'default_model','local_declaration')
+    if (length(object@model_code) > 0) {
+          tmp <- stan_model(model_code = object@model_code, ...)
+          object@path  <- ifelse(object@default_model,'default_model','local_declaration')
     } else {
-        stop('compilation error; check .Object@path and .Object@model_code\n')
+        stop('compilation error; check object@path and object@model_code\n')
     }
   }
   
-  .Object@model_cpp  <- tmp@model_cpp
-  .Object@dso        <- tmp@dso
+  object@model_cpp  <- tmp@model_cpp
+  object@dso        <- tmp@dso
   
-  .Object
+  object
   
 }
