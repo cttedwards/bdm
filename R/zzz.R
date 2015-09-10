e9dff9813bd201d58d20dbf15f231a1a1e572917
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-10 18:12:05 +1200

    minimum working package

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname)
+{
+    packageStartupMessage("bdm beta version 0.0.0.9000 (2015-09-10 17:45:13)")
+}
--- tests/testthat/test_compiler.R
+++ tests/testthat/test_compiler.R
@@ -3,25 +3,28 @@ context("compiler")
 
 test_that("rstan compilation", {
     # initialize code directly
-    expect_error(toy(model.code = "parameters {real y;} model {y ~ normal(0,1);}"))
+    expect_error(bdm(model.code = "parameters {real y;} model {y ~ normal(0,1);}"))
     # must provide a model name
-    mdl <- toy(model.code = "parameters {real y;} model {y ~ normal(0,1);}", model.name = "toy_model")
+    mdl <- bdm(model.code = "parameters {real y;} model {y ~ normal(0,1);}", model.name = "toy_model")
     mdl <- compiler(mdl)
-    expect_is(mdl, "toy")
+    expect_is(mdl, "bdm")
+    expect_equal(mdl@model_name, "toy_model")
     expect_is(mdl@dso, "cxxdso")
     expect_true(mdl@dso@dso_saved)
     # initialize code indirectly
     # model name generated automatically
     toy_model <- "parameters {real y;} model {y ~ normal(0,1);}"
-    mdl <- toy(model.code = toy_model)
+    mdl <- bdm(model.code = toy_model)
     mdl <- compiler(mdl)
-    expect_is(mdl, "toy")
+    expect_is(mdl, "bdm")
+    expect_equal(mdl@model_name, "toy_model")
     expect_is(mdl@dso, "cxxdso")
     expect_true(mdl@dso@dso_saved)
     # default initialization
-    mdl <- toy()
+    mdl <- bdm()
     mdl <- compiler(mdl)
-    expect_is(mdl, "toy")
+    expect_is(mdl, "bdm")
+    expect_equal(mdl@model_name, "default")
     expect_is(mdl@dso, "cxxdso")
     expect_true(mdl@dso@dso_saved)
 })
