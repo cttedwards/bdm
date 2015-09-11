9e56635920cc702dd7a25d2279f0de64fd27476f
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-11 12:19:00 +1200

    added slots to bdm-class
    init.values and trace_array

--- R/bdm-class.R
+++ R/bdm-class.R
@@ -4,12 +4,14 @@
 #'
 #' @slot data a \code{list} of input data
 #' @slot init a \code{function} which must return initial values
+#' @slot init.values List of initial values populated by \code{init}.
 #' @slot chains the number of chains
 #' @slot iter the number of samples for each chain
 #' @slot warmup the 'burnin' period for each chain
 #' @slot thin the period of sampling from each chain
 #' @slot nsamples the resultant number of samples retained from the sample run
 #' @slot trace \code{list} holding the processed samples from \code{\link[rstan:extract]{extract(..., permuted = TRUE, inc_warmup = FALSE)}}.
+#' @slot trace_array \code{array} holding the processed samples from \code{\link[rstan:extract]{extract(..., permuted = FALSE, inc_warmup = TRUE)}}. This should rarely be needed for direct access but is used for diagnostic plots.
 #' @slot model_name \code{character} string giving the model name.
 #' @slot model_code the model code in the Stan modelling language.
 #' @slot model_cpp translation of Stan code into C++.
@@ -24,12 +26,15 @@
 setClass("bdm",contains = "stanmodel",
          slots = list(data = "list",          # edat object or list
                     init = "function",        # initialisation function
+                    init.values = "list",     # initial values populated by init.func
                     chains = "numeric",       # number of MCMC chains
                     iter = "numeric",         # number of MCMC iterations per chain
                     warmup = "numeric",       # number of iterations under adaptive sampling
                     thin = "numeric",         # interval between recorded samples
                     nsamples = "numeric",     # total number of posterior samples recorded
-                    trace = "list"            # list of posterior samples without warmup and with chains mixed
+                    trace = "list",           # list of posterior samples without warmup and with chains mixed
+                    trace_array = "array",    # array of posterior samples including warmup
+                    run = "character"         # optional label for this particular run
          )
 )
 #}}}
