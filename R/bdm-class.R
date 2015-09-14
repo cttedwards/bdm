6078fb51e9936ca737b17950761667cc0446e439
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-14 14:48:44 +1200

    removed init slot in bdm class
    need to simplify to make compatible with default 'random' initialisation
    of rstan::sampling

--- R/bdm-class.R
+++ R/bdm-class.R
@@ -3,8 +3,7 @@
 #' This is the primary object class for the \pkg{bdm} package. It inherits from the \code{\link[rstan:stanmodel-class]{stanmodel}} superclass.
 #'
 #' @slot data a \code{list} of input data
-#' @slot init a \code{function} which must return initial values
-#' @slot init.values List of initial values populated by \code{init}.
+#' @slot inits a \code{list} of initial values
 #' @slot chains the number of chains
 #' @slot iter the number of samples for each chain
 #' @slot warmup the 'burnin' period for each chain
@@ -25,8 +24,7 @@
 #{{{
 setClass("bdm",contains = "stanmodel",
          slots = list(data = "list",          # edat object or list
-                    init = "function",        # initialisation function
-                    init.values = "list",     # initial values populated by init.func
+                    inits = "list",           # initial values populated by initialisation function
                     chains = "numeric",       # number of MCMC chains
                     iter = "numeric",         # number of MCMC iterations per chain
                     warmup = "numeric",       # number of iterations under adaptive sampling
