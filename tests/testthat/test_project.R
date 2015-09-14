91696aa4a0254ccb8037ba30b5eab9a6b90597e0
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-15 11:19:31 +1200

    simplified test_project
    set number of chains = 1

--- tests/testthat/test_project.R
+++ tests/testthat/test_project.R
@@ -9,21 +9,21 @@ test_that("project", {
     # initialize and fit default model
     mdl <- bdm()
     capture.output(mdl <- compiler(mdl))
-    capture.output(mdl <- sampler(mdl, dat, run = 'example_run'))
+    capture.output(mdl <- sampler(mdl, dat, chains = 1, run = 'example_run'))
 
     # constant harvest rate projection scenario
     mdl.project <- project(mdl, harvest = 0.10, time = 10, harvest_rate = TRUE)
     
     expect_is(mdl.project, "list")
     expect_equal(mdl@run, mdl.project$run)
-    expect_equal(mdl.project$harvest_rate[4000, 48, 1], 0.1)
+    expect_equal(mdl.project$harvest_rate[1000, 48, 1], 0.1)
     
     # constant harvest projection scenario
     mdl.project <- project(mdl, harvest = 1000, time = 10, harvest_rate = FALSE)
     
     expect_is(mdl.project, "list")
     expect_equal(mdl@run, mdl.project$run)
-    expect_equal(mdl.project$harvest[4000, 48, 1], 1000)
+    expect_equal(mdl.project$harvest[1000, 48, 1], 1000)
 })
 
 
