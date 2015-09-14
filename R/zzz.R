8a8b2ae204833fcf457e870ab1e2c40b92b43251
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-14 14:49:29 +1200

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("bdm beta version 0.0.0.9003 (2015-09-11 17:05:01)")
+    packageStartupMessage("bdm beta version 0.0.0.9004 (2015-09-14 14:43:15)")
 }
