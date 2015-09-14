4a3e4a7b9314d67db4955193279b1e47c4893bb4
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-14 17:25:14 +1200

    fixed get functions

--- R/getcpp.R
+++ R/getcpp.R
@@ -20,24 +20,3 @@ getcpp.bdm <- function(object, ...) {
     
     return(cpp)
 }
-
-#'
-#' Return model parameters (including input data)
-#' 
-#' @param object a \code{bdm} class object 
-#' @param ... additional arguments to generic function
-#' 
-#' @export
-getparams <- function(object, ...) UseMethod("getparams")
-#'
-#' @rdname getparams
-#' @export
-getparams.bdm <- function(object, ...) {
-    
-    cpp <- getcpp(object)
-    
-    params <- grep("vals_r__ = context__.vals_r(", fixed = TRUE, value = TRUE, x = strsplit(cpp, "\n")[[1]])
-    params <- sapply(strsplit(params, "\""), FUN = function(x) x[[2]])
-    
-    return(params)
-}
