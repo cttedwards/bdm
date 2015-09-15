09627f74c6252f64ca839f00a64de85311b6ac6a
Charles Edwards <charles.edwards@niwa.co.nz>
2015-09-15 13:21:50 +1200

    rebuild
    version bdm_0.0.0.9008 for distribution

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("bdm beta version 0.0.0.9007 (2015-09-15 11:18:31)")
+    packageStartupMessage("bdm beta version 0.0.0.9008 (2015-09-15 12:28:34)")
 }
--- build_bdm.R
+++ build_bdm.R
@@ -32,7 +32,18 @@ devtools::run_examples(fresh = TRUE)
 # run tests
 devtools::test()
 
-# build and install
+# generate vignettes
+devtools::build_vignettes()
+
+# final check
+# (temporarily remove test folder to avoid repetition)
+devtools::check(vignettes = FALSE)
+
+# build and install to R library
 system("Rcmd.exe INSTALL --no-multiarch --with-keep.source ../bdm")
+
+# build local binary
+devtools::build(binary = TRUE)
+
 
 
