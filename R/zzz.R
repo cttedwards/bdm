a89c27c0fd9483c4024e68b8e9c92e1478fc28c4
Charles <cescapecs@gmail.com>
2021-02-18 12:19:45 +0300

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("bdm beta version 0.0.0.9034 (2020-09-16 08:21:54)")
+    packageStartupMessage("bdm beta version 0.0.0.9035 (2020-09-18 10:08:00)")
 }
